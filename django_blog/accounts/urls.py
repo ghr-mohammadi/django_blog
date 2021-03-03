8c8b8263e27e5660c31b335e628592d89458c03b
gholamrezaMohammadi <gholamreza13720224@gmail.com>
2021-03-03 23:39:07 +0330

    password change form and password change done added

--- django_blog/accounts/urls.py
+++ django_blog/accounts/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('signup/', views.signup, name='signup')
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('signup/', views.signup, name='signup'),
+    path('delete/', views.delete, name='delete')
 ]
--- django_blog/accounts/views.py
+++ django_blog/accounts/views.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import Group
+from django.contrib import messages
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -15,7 +17,20 @@ def signup(request):
             blog_user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
             blog_user.groups.add(Group.objects.get(name='ساده'))
             login(request=request, user=blog_user)
+            messages.success(request, "حساب کاربری با موفقیت ایجاد شد.")
             return HttpResponseRedirect(reverse('blog:home'))
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+
+@login_required
+def delete(request):
+    try:
+        user = request.user
+        user.delete()
+        messages.success(request, "حساب کاربری با موفقیت حذف شد.")
+    except:
+        messages.error(request, "حذف حساب کاربری با خطا موافق شد.")
+
+    return HttpResponseRedirect(reverse('blog:home'))
